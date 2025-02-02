aa7bb3b0f4ac17edfe5bfc57033e4b8cbf992fc1
Dr-Dee-pak <deep786786@gmail.com>
2025-02-02 18:32:13 +0530

    changed the header footer and blog tittles.

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,14 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'THE USABUZZ';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Trend That Trends';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : '© 2023 THE USABUZZ. ALL RIGHTS RESERVED.\n' +
+      'Unauthorized use, reproduction, or distribution of any content, materials, or intellectual property owned by THE USABUZZ is strictly prohibited without prior written consent. All trademarks, logos, and service marks displayed on this platform are the property of their respective owners. THE USABUZZ reserves the right to take legal action against any infringement of its intellectual property rights. For inquiries regarding permissions or usage, please contact legal@https://www.usabuzz.netlify.app';
 
   return {
     name,

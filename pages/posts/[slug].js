1a7bf72fc5b9307ac841abc6c978a381fa073a9d
DeepakDubeyCS <145714088+DeepakDubeyCS@users.noreply.github.com>
2025-02-02 12:31:15 +0530

    Update [slug].js
    fixing the error

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,9 +1,9 @@
 import { getGlobalData } from '../../utils/global-data';
 import {
-    getNextPostBySlug,
-    getPostBySlug,
-    getPreviousPostBySlug,
-    getPostFilePaths,
+  getNextPostBySlug,
+  getPostBySlug,
+  getPreviousPostBySlug,
+  getPostFilePaths,
 } from '../../utils/mdx-utils';
 
 import { MDXRemote } from 'next-mdx-remote';
@@ -18,137 +18,122 @@ import Layout, { GradientBackground } from '../../components/Layout';
 import SEO from '../../components/SEO';
 
 // Custom components/renderers to pass to MDX.
+// Since the MDX files aren't loaded by webpack, they have no knowledge of how
+// to handle import statements. Instead, you must include components in scope
+// here.
 const components = {
-    a: CustomLink,
-    Head,
-    img: CustomImage,
+  a: CustomLink,
+  // It also works with dynamically-imported components, which is especially
+  // useful for conditionally loading components for certain routes.
+  // See the notes in README.md for more details.
+  Head,
+  img: CustomImage
 };
 
 export default function PostPage({
-                                     source,
-                                     frontMatter,
-                                     prevPost,
-                                     nextPost,
-                                     globalData,
-                                     slug,
-                                 }) {
-    return (
-        <Layout>
-            <SEO
-                title={`${frontMatter.title} - ${globalData.name}`}
-                description={frontMatter.description}
-            />
-            <Header name={globalData.name} />
-            <article className="px-6 md:px-0" data-sb-object-id={`posts/${slug}.mdx`}>
-                <header>
-                    <h1 className="mb-12 text-3xl text-center md:text-5xl dark:text-white" data-sb-field-path="title">
-                        {frontMatter.title}
-                    </h1>
-                    {frontMatter.description && (
-                        <p className="mb-4 text-xl" data-sb-field-path="description">{frontMatter.description}</p>
-                    )}
-                </header>
-                <main>
-                    <article className="prose dark:prose-dark" data-sb-field-path="markdown_content">
-                        <MDXRemote {...source} components={components} />
-                    </article>
-                </main>
-                <div className="grid mt-12 md:grid-cols-2 lg:-mx-24">
-                    {prevPost && (
-                        <Link
-                            href={`/posts/${prevPost.slug}`}
-                            className="flex flex-col px-10 py-8 text-center transition bg-white border border-gray-800 md:text-right first:rounded-t-lg md:first:rounded-tr-none md:first:rounded-l-lg last:rounded-r-lg first last:rounded-b-lg backdrop-blur-lg dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 dark:border-white border-opacity-10 dark:border-opacity-10 last:border-t md:border-r-0 md:last:border-r md:last:rounded-r-none"
-                        >
-                            <p className="mb-4 text-gray-500 uppercase dark:text-white dark:opacity-60">
-                                Previous
-                            </p>
-                            <h4 className="mb-6 text-2xl text-gray-700 dark:text-white">
-                                {prevPost.title}
-                            </h4>
-                            <ArrowIcon className="mx-auto mt-auto transform rotate-180 md:mr-0" />
-                        </Link>
-                    )}
-                    {nextPost && (
-                        <Link
-                            href={`/posts/${nextPost.slug}`}
-                            className="flex flex-col px-10 py-8 text-center transition bg-white border border-t-0 border-b-0 border-gray-800 md:text-left md:first:rounded-t-lg last:rounded-b-lg first:rounded-l-lg md:last:rounded-bl-none md:last:rounded-r-lg backdrop-blur-lg dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 dark:border-white border-opacity-10 dark:border-opacity-10 first:border-t first:rounded-t-lg md:border-t last:border-b"
-                        >
-                            <p className="mb-4 text-gray-500 uppercase dark:text-white dark:opacity-60">
-                                Next
-                            </p>
-                            <h4 className="mb-6 text-2xl text-gray-700 dark:text-white">
-                                {nextPost.title}
-                            </h4>
-                            <ArrowIcon className="mx-auto mt-auto md:ml-0" />
-                        </Link>
-                    )}
-                </div>
-            </article>
-            <Footer copyrightText={globalData.footerText} />
-            <GradientBackground
-                variant="large"
-                className="absolute -top-32 opacity-30 dark:opacity-50"
-            />
-            <GradientBackground
-                variant="small"
-                className="absolute bottom-0 opacity-20 dark:opacity-10"
-            />
-        </Layout>
-    );
+  source,
+  frontMatter,
+  prevPost,
+  nextPost,
+  globalData,
+  slug
+}) {
+
+
+  return (
+    <Layout>
+      <SEO
+        title={`${frontMatter.title} - ${globalData.name}`}
+        description={frontMatter.description}
+      />
+      <Header name={globalData.name} />
+      <article className="px-6 md:px-0" data-sb-object-id={`posts/${slug}.mdx`}>
+        <header>
+          <h1 className="mb-12 text-3xl text-center md:text-5xl dark:text-white" data-sb-field-path="title">
+            {frontMatter.title}
+          </h1>
+          {frontMatter.description && (
+            <p className="mb-4 text-xl" data-sb-field-path="description">{frontMatter.description}</p>
+          )}
+        </header>
+        <main>
+          <article className="prose dark:prose-dark" data-sb-field-path="markdown_content">
+            <MDXRemote {...source} components={components} />
+          </article>
+        </main>
+        <div className="grid mt-12 md:grid-cols-2 lg:-mx-24">
+          {prevPost && (
+            (<Link
+              href={`/posts/${prevPost.slug}`}
+              className="flex flex-col px-10 py-8 text-center transition bg-white border border-gray-800 md:text-right first:rounded-t-lg md:first:rounded-tr-none md:first:rounded-l-lg last:rounded-r-lg first last:rounded-b-lg backdrop-blur-lg dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 dark:border-white border-opacity-10 dark:border-opacity-10 last:border-t md:border-r-0 md:last:border-r md:last:rounded-r-none">
+
+              <p className="mb-4 text-gray-500 uppercase dark:text-white dark:opacity-60">
+                Previous
+              </p>
+              <h4 className="mb-6 text-2xl text-gray-700 dark:text-white">
+                {prevPost.title}
+              </h4>
+              <ArrowIcon className="mx-auto mt-auto transform rotate-180 md:mr-0" />
+
+            </Link>)
+          )}
+          {nextPost && (
+            (<Link
+              href={`/posts/${nextPost.slug}`}
+              className="flex flex-col px-10 py-8 text-center transition bg-white border border-t-0 border-b-0 border-gray-800 md:text-left md:first:rounded-t-lg last:rounded-b-lg first:rounded-l-lg md:last:rounded-bl-none md:last:rounded-r-lg backdrop-blur-lg dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 dark:border-white border-opacity-10 dark:border-opacity-10 first:border-t first:rounded-t-lg md:border-t last:border-b">
+
+              <p className="mb-4 text-gray-500 uppercase dark:text-white dark:opacity-60">
+                Next
+              </p>
+              <h4 className="mb-6 text-2xl text-gray-700 dark:text-white">
+                {nextPost.title}
+              </h4>
+              <ArrowIcon className="mx-auto mt-auto md:ml-0" />
+
+            </Link>)
+          )}
+        </div>
+      </article>
+      <Footer copyrightText={globalData.footerText} />
+      <GradientBackground
+        variant="large"
+        className="absolute -top-32 opacity-30 dark:opacity-50"
+      />
+      <GradientBackground
+        variant="small"
+        className="absolute bottom-0 opacity-20 dark:opacity-10"
+      />
+    </Layout>
+  );
 }
 
 export const getStaticProps = async ({ params }) => {
-    const globalData = getGlobalData();
-    const { mdxSource, data } = await getPostBySlug(params.slug);
+  const globalData = getGlobalData();
+  const { mdxSource, data } = await getPostBySlug(params.slug);
+  const prevPost = getPreviousPostBySlug(params.slug);
+  const nextPost = getNextPostBySlug(params.slug);
 
-    // Validate post data
-    if (!data || !data.title || !data.slug) {
-        return {
-            notFound: true, // Return a 404 page if the post is invalid
-        };
-    }
-
-    const prevPost = getPreviousPostBySlug(params.slug);
-    const nextPost = getNextPostBySlug(params.slug);
-
-    // Ensure all properties of prevPost and nextPost are serializable
-    const prevPostData = prevPost
-        ? {
-            ...prevPost,
-            title: prevPost.title || null, // Replace undefined title with null
-            slug: prevPost.slug || null, // Replace undefined slug with null
-        }
-        : null;
-
-    const nextPostData = nextPost
-        ? {
-            ...nextPost,
-            title: nextPost.title || null, // Replace undefined title with null
-            slug: nextPost.slug || null, // Replace undefined slug with null
-        }
-        : null;
-
-    return {
-        props: {
-            globalData,
-            source: mdxSource,
-            frontMatter: data,
-            slug: params.slug,
-            prevPost: prevPostData, // Assign null if undefined
-            nextPost: nextPostData, // Assign null if undefined
-        },
-    };
+  return {
+    props: {
+      globalData,
+      source: mdxSource,
+      frontMatter: data,
+      slug: params.slug,
+      prevPost,
+      nextPost,
+    },
+  };
 };
 
 export const getStaticPaths = async () => {
-    const paths = getPostFilePaths()
-        // Remove file extensions for page paths
-        .map((path) => path.replace(/\.mdx?$/, ''))
-        // Map the path into the static paths object required by Next.js
-        .map((slug) => ({ params: { slug } }));
+  const paths = getPostFilePaths()
+    // Remove file extensions for page paths
+    .map((path) => path.replace(/\.mdx?$/, ''))
+    // Map the path into the static paths object required by Next.js
+    .map((slug) => ({ params: { slug } }));
 
-    return {
-        paths,
-        fallback: false,
-    };
+  return {
+    paths,
+    fallback: false,
+  };
 };
